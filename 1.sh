91aa527276edbf3287ebc58c1768d7bf0a8e2e81
HarshalGundaCuelogic <harshal.gunda@cuelogic.com>
2019-09-10 18:21:47 +0530

    left with install openssh-server on both the containers then install openrc and openrc --no-cache

--- 1.sh
+++ 1.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sh ./server/fileserver.sh && sh ./client/fileclient.sh
+sh ./server/fileserver.sh && sh ./client/fileclient.sh;
 #sudo docker run -dp 8088:80 --name=ServerContainer --mount source=servervol,target=/serverdata --network=mynetwork1 --ip=172.168.11.42 serverimage:1.0
 #sudo docker run -dp 8089:80 --name=ClientContainer --mount source=clientvol,target=/clientdata --network=mynetwork1 --ip=172.168.11.43 clientimage:1.0
